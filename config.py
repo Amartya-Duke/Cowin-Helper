59873041c78a72f26ac96f6d84f48522bede8607
Amartya <amartya.2k9@gmail.com>
2021-05-16 18:20:43 +0530

    remove sensitive information

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 USE_PUBLIC_API = True
-PHONE_NUMBER = '9629778910'
+PHONE_NUMBER = '***'
 BROWSER_PATH = '"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" %s'  # it will be used to open a browser tab pointing to cowin for registration, if slots becomes available
 
 SLOT_CONFIG = {
@@ -15,3 +15,4 @@ TWILIO_AUTH_TOKEN = "***"
 TWILIO_SENDER_NUMBER = "***"
 
 SECRET = "***"
+
